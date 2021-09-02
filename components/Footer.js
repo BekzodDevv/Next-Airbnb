7647f4bd132a4c31c184e90b2c9d91de09f600be
BekzodDevv <bekzodabdulla04@gmail.com>
2021-09-02 21:25:07 +0500

    version1

---
+++ components/Footer.js
@@ -0,0 +1,60 @@
+
+
+
+
+function Footer() {
+    return (
+        <div className="grid grid-cols-1 text-center md:grid-cols-4 md:text-left gap-y-10 px-32
+        py-14 bg-gray-100 text-gray-600">
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">ABOUT</h5>
+                <p>How Airbnb works</p>
+                <p>Newsroom</p>
+                <p>This is not a real clone</p>
+                <p>Its a pretty awesome clone</p>
+                <p>Referrals accepted</p>
+                <p>Papafam</p>
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">COMMUNITY</h5>
+                <p>How Airbnb works</p>
+                <p>Newsroom</p>
+                <p>This is not a real clone</p>
+                <p>Its a pretty awesome clone</p>
+                <p>Referrals accepted</p>
+                <p>Papafam</p>
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">HOST</h5>
+                <p>How Airbnb works</p>
+                <p>Newsroom</p>
+                <p>This is not a real clone</p>
+                <p>Its a pretty awesome clone</p>
+                <p>Referrals accepted</p>
+                <p>Papafam</p>
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">ABOUT</h5>
+                <p>How Airbnb works</p>
+                <p>Newsroom</p>
+                <p>This is not a real clone</p>
+                <p>Its a pretty awesome clone</p>
+                <p>Referrals accepted</p>
+                <p>Papafam</p>
+            </div>
+
+
+
+
+
+
+
+
+        </div>
+    )
+}
+
+export default Footer
